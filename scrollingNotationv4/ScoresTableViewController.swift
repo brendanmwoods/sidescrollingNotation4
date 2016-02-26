af97117544d91c9ea7bbe87c0d73ac9eed7b92e4
brendan woods <brendanwoods@brendans-MacBook-Air.local>
2016-02-25 20:43:11 -0800

    more gui changes

--- scrollingNotationv4/ScoresTableViewController.swift
+++ scrollingNotationv4/ScoresTableViewController.swift
@@ -242,90 +242,115 @@ class ScoresTableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-    
-        for  var i = 0; i <= tableView.numberOfSections; i++ {
+        
+        
+        //for  var i = 0; i <= tableView.numberOfSections; i++ {
+        
+        
         //tableView.headerViewForSection(i)?.contentView.backgroundColor = UIColor.blackColor()
         //tableView.headerViewForSection(i)?.textLabel?.textColor = UIColor.whiteColor()
-        tableView.headerViewForSection(i)?.textLabel?.textAlignment = .Center
-        }
+        //tableView.headerViewForSection(i)?.textLabel?.textAlignment = .Center
+        
+        //return header
+        // }
     }
     
-    
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let title: UILabel = UILabel()
+        
+        title.text = "test label"
+        title.textColor = UIColor.whiteColor()
+        title.backgroundColor = UIColor.blackColor()
+        title.textAlignment = .Center
+        
         if section == 0 {
-            return "High Score"
-        }else if section == 1 {
-            return "Average Score"
+            title.text = "High Score"
+        }  else if section == 1 {
+            title.text = "Average Score"
         }else if section == 2 {
-            return "Total Games Played"
+            title.text = "Total Games Played"
         }else if section == 3{
-            return "Recent Scores"
-        }else {
-            return " "
+            title.text = "Recent Scores"
         }
+        return title
     }
-    
-    
-    
-    //    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    //        // #warning Incomplete implementation, return the number of rows
-    //        return 0
-    //    }
-    
-    
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-    
-    // Configure the cell...
-    
-    return cell
-    }
-    */
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
-    return true
-    }
-    */
-    
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-    if editingStyle == .Delete {
-    // Delete the row from the data source
-    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-    } else if editingStyle == .Insert {
-    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }
-    }
-    */
-    
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-    
-    }
-    */
-    
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the item to be re-orderable.
-    return true
-    }
-    */
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
-    
 }
+
+//    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        if section == 0 {
+//            return "High Score"
+//        }else if section == 1 {
+//            return "Average Score"
+//        }else if section == 2 {
+//            return "Total Games Played"
+//        }else if section == 3{
+//            return "Recent Scores"
+//        }else {
+//            return " "
+//        }
+
+
+
+
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+
+
+/*
+override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+// Configure the cell...
+
+return cell
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+// Return false if you do not want the specified item to be editable.
+return true
+}
+*/
+
+/*
+// Override to support editing the table view.
+override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+if editingStyle == .Delete {
+// Delete the row from the data source
+tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+} else if editingStyle == .Insert {
+// Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+}
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+// Return false if you do not want the item to be re-orderable.
+return true
+}
+*/
+
+/*
+// MARK: - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+// Get the new view controller using segue.destinationViewController.
+// Pass the selected object to the new view controller.
+}
+*/
+
+
