3ad50b4c8929811dde2981145a9944587380b962
jakhamma <woodybrendan@hotmail.com>
2016-03-10 16:54:11 -0800

    login authorization is working as expected. once logged in, the login view controllers are popped of stack. going to make gui changes to dark colours now

---
+++ scrollingNotationv4/CreateAccountViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateAccountViewController.swift
+//  scrollingNotationv4
+//
+//  Created by brendan woods on 2016-03-09.
+//  Copyright © 2016 brendan woods. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
