599c1b57bd286273bab5266ba61ff543832aaad4
jakhamma <woodybrendan@hotmail.com>
2016-03-16 12:34:15 -0700

    all views match now

--- scrollingNotationv4/LearningMenuViewController.swift
+++ scrollingNotationv4/LearningMenuViewController.swift
@@ -20,6 +20,10 @@ class LearningMenuViewController: UIViewController {
         let backItem = UIBarButtonItem(title: "Menu", style: UIBarButtonItemStyle.Plain, target: nil, action: nil)
         navigationItem.backBarButtonItem = backItem
     }
+    override func viewDidAppear(animated: Bool) {
+        self.navigationController!.navigationBar.translucent = true
+        self.navigationController!.navigationBar.backgroundColor = UIColor.clearColor()
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
